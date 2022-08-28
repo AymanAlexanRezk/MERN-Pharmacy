cb98f20e4469568babd50556b5c4bd6723af9f10
AymanAlexanRezk <memo20092000@gmail.com>
2022-08-28 21:40:36 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Ayman Pharmacy</a>
+        <p>Your Choice For Health & Beauty</p>
+      </header>
+
+      <main>Products</main>
+
+      <footer>Copyright ©2022 Ayman Pharmacy | Hurghada</footer>
+    </div>
+  );
+}
+
+export default App;
