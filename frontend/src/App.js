e6645886f10ce6c03b4b2366f5eb43ee6ac9d60d
AymanAlexanRezk <memo20092000@gmail.com>
2022-08-29 12:24:10 +0200

    add responsive home screen for products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from './data';
+
 function App() {
   return (
     <div className="App">
@@ -6,7 +8,22 @@ function App() {
         <p>Your Choice For Health & Beauty</p>
       </header>
 
-      <main>Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <img src={product.imgUrl} alt={product.name} />
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+              <p>
+                <strong>{product.price} £E</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          ))}
+        </div>
+      </main>
 
       <footer>Copyright ©2022 Ayman Pharmacy | Hurghada</footer>
     </div>
---
+++ frontend/src/data.js
@@ -0,0 +1,68 @@
+const data = {
+  products: [
+    {
+      name: 'BOBAI Sunscreen Cream SPF80 50gm',
+      slug: 'bobai-sunscreen-cream-spf-80',
+      description:
+        'Rich Texture, Antioxidant, Invisible Texture, Hypoallergenic',
+      category: 'Sunscreen',
+      //* Don't use './images/image.jpg' => current folder is changeable.
+      imgUrl: '/images/bobai-sunscreen-cream-spf80.png',
+      brand: 'Parkville',
+      countInStock: 5,
+      price: 80,
+      rating: 4.9,
+      numReviews: 5,
+    },
+    {
+      name: 'MONDO Cream 50gm',
+      slug: 'mondo-cream',
+      description: 'Topical cream for skin care, repair and protection',
+      category: 'Moisturizer',
+      imgUrl: '/images/mondo-cream.png',
+      brand: 'MACRO',
+      countInStock: 5,
+      price: 55,
+      rating: 4.9,
+      numReviews: 5,
+    },
+    {
+      name: 'Macro Panthenol Cream 50gm',
+      slug: 'macro-panthenol-cream',
+      description: 'Skin cream for softening, repair & protection',
+      category: 'Moisturizer',
+      imgUrl: '/images/macro-panthenol-cream.png',
+      brand: 'MACRO',
+      countInStock: 5,
+      price: 35,
+      rating: 4.9,
+      numReviews: 5,
+    },
+    {
+      name: 'Macro Panthenol Cream 50gm..',
+      slug: 'macro-panthenol-cream..',
+      description: 'Skin cream for softening, repair & protection',
+      category: 'Moisturizer',
+      imgUrl: '/images/macro-panthenol-cream.png',
+      brand: 'MACRO',
+      countInStock: 5,
+      price: 35,
+      rating: 4.9,
+      numReviews: 5,
+    },
+    {
+      name: 'Macro Panthenol Cream 50gm.',
+      slug: 'macro-panthenol-cream.',
+      description: 'Skin cream for softening, repair & protection',
+      category: 'Moisturizer',
+      imgUrl: '/images/macro-panthenol-cream.png',
+      brand: 'MACRO',
+      countInStock: 5,
+      price: 35,
+      rating: 4.9,
+      numReviews: 5,
+    },
+  ],
+};
+
+export default data;
